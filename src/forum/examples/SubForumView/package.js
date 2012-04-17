58f9575e9e14f3a04e417869235573740660c72e
straider <straider@straiderBB.(none)>
2012-04-17 17:10:22 +0200

    Made an example how to use SubForumView.

---
+++ src/forum/examples/SubForumView/package.js
@@ -0,0 +1,9 @@
+enyo.depends(
+    "/socket.io/socket.io.js",
+    "api.js",
+    "../../forumdatabase.js",
+    "../../style.css",
+    "../../lib/onyx",
+    "../../kinds",
+    "../../lib/extra/jsonp"
+);
