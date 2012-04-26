86fb3c63bad748b0df7fd6a304363af1d64ba8d0
robud948 <robud948@student.liu.se>
2012-04-26 20:58:22 +0200

    added more Language support

--- src/forum/Language.js
+++ src/forum/Language.js
@@ -6,43 +6,61 @@ Language.DEFAULTLANG = "en";
 
 Language.l = function( string, lang ) {
 
-	var getLang;
-	if ( lang !== undefined ) {
-		getLang = lang;
-	} else {
-		getLang = Language.DEFAULTLANG;
-	}
+    var getLang;
+    if ( lang !== undefined ) {
+	getLang = lang;
+    } else {
+	getLang = Language.DEFAULTLANG;
+    }
 
-	var got;
-	got = this.languages[getLang][string];
+    var got;
+    got = this.languages[getLang][string];
 
-	if ( got === undefined ) {
-		if ( getLang === Language.DEFAULTLANG ) {
-			return 'Could not find "' + string + '"';
-		}
-		got = this.l( string, Language.DEFAULTLANG );
+    if ( got === undefined ) {
+	if ( getLang === Language.DEFAULTLANG ) {
+	    return 'Could not find "' + string + '"';
 	}
-	return got;
+	got = this.l( string, Language.DEFAULTLANG );
+    }
+    return got;
 }
 
 Language.languages = {
-	en: {
-		by: "by",
-		lastPost: "last post",
-		thread: "thread",
-		topic: "topic",
-		post: "post",
-		posts: "posts",
-		startedBy: "started by"
-	},
+    en: {
+	reply: "reply",
+	by: "by",
+	post: "post",	
+	thread: "thread",
+	//ThreadContainer
+	topic: "topic",
+	startedBy: "started by",
+	lastPost: "last post",
+	posts: "posts",
+	//CategoryView
+	forum: "forum",
+	threads: "threads",
+	newThreads: "new threads",
+	newCategory: "new category",
+	//SubForumView
+	newSubForum: "new subforum"
+    },
 
-	sv: {
-		by: "av",
-		lastPost: "senaste posten",
-		thread: "tråd",
-		topic: "ämne",
-		post: "inlägg",
-		posts: "inlägg",
-		startedBy: "startad av"
-	}
+    sv: {
+	reply: "svar",
+	by: "av",
+	post: "inlägg",
+	thread: "tråd",
+	//ThreadContainer
+	topic: "ämne",
+	startedBy: "startad av",
+	lastPost: "senaste posten",
+	posts: "inlägg",
+	//CategoryView
+	forum: "forum",
+	threads:  "trådar",
+	newThreads: "nya trådar",
+	newCategory: "Ny Kategori",
+	//SubForumView
+	newSubForum: "Nytt Subforum"
+    }
 }
